3fb6f35feac2ad0cc9a270401bf55fdb1d399462
rabdiego <diegorabelodesa@gmail.com>
2023-10-10 20:24:56 -0300

    ROGER

--- main.cpp
+++ main.cpp
@@ -26,11 +26,11 @@ int main(int argc, char* argv[])
 	Eigen::Vector3d origin(0, 0, 0);
 
 	// Parâmetros da janela e canvas
-	int windowDistance = 30;
-	int windowWidth = 500;
-	int windowHeight = 500;
-	int numColumns = 500;
-	int numLines = 500;
+	int windowDistance = 1000;
+	int windowWidth = 600;
+	int windowHeight = 600;
+	int numColumns = 600;
+	int numLines = 600;
 
 	// Criação da janela
 	SDL_Window* window = SDL_CreateWindow
@@ -63,17 +63,74 @@ int main(int argc, char* argv[])
 	// Canvas e cena
 	Canvas canvas(windowDistance, windowWidth, windowHeight, numLines, numColumns);
 	Scene scene;
+
+	double bodyRadius = 100;
+	Eigen::Vector3d bodyCenter(0, -100, -1100);
+	Eigen::Vector3d bodyKAmbient(1, 1, 1);
+	Eigen::Vector3d bodyKDif(1, 1, 1);
+	Eigen::Vector3d bodyKEsp(0, 0, 0);
+	Object* body = new Sphere(bodyRadius, bodyCenter, bodyKAmbient, bodyKDif, bodyKEsp, 0);
+
+	double headRadius = 75;
+	Eigen::Vector3d headCenter(0, 35, -1100);
+	Eigen::Vector3d headKAmbient(1, 1, 1);
+	Eigen::Vector3d headKDif(1, 1, 1);
+	Eigen::Vector3d headKEsp(0, 0, 0);
+	Object* head = new Sphere(headRadius, headCenter, headKAmbient, headKDif, headKEsp, 0);
+
+	double leftEyeRadius = 20;
+	Eigen::Vector3d leftEyeCenter(-35, 55, -1045);
+	Eigen::Vector3d leftEyeKAmbient(0, 0.4, 1);
+	Eigen::Vector3d leftEyeKDif(0, 0.4, 1);
+	Eigen::Vector3d leftEyeKEsp(0.3, 0.3, 0.3);
+	Object* leftEye = new Sphere(leftEyeRadius, leftEyeCenter, leftEyeKAmbient, leftEyeKDif, leftEyeKEsp, 1);
+
+	double rightEyeRadius = 20;
+	Eigen::Vector3d rightEyeCenter(35, 55, -1045);
+	Eigen::Vector3d rightEyeKAmbient(0, 0.4, 1);
+	Eigen::Vector3d rightEyeKDif(0, 0.4, 1);
+	Eigen::Vector3d rightEyeKEsp(0.3, 0.3, 0.3);
+	Object* rightEye = new Sphere(rightEyeRadius, rightEyeCenter, rightEyeKAmbient, rightEyeKDif, rightEyeKEsp, 2);
 	
-	double sphereRadius = 90;
-	Eigen::Vector3d sphereCenter(0, 0, -100);
-	Eigen::Vector3d sphereKAmbient(0.7, 0.2, 0.2);
-	Eigen::Vector3d sphereKDif(0.7, 0.2, 0.2);
-	Eigen::Vector3d sphereKEsp(0.7, 0.2, 0.2);
-	int sphereSpecularIndex = 10;
-	Object* sphere = new Sphere(sphereRadius, sphereCenter, sphereKAmbient, sphereKDif, sphereKEsp, sphereSpecularIndex);
+	double upperRightMouthRadius = 10;
+	Eigen::Vector3d upperRightMouthCenter(-32, 15, -1005);
+	Eigen::Vector3d upperRightMouthKAmbient(0.4, 0.2, 0);
+	Eigen::Vector3d upperRightMouthKDif(0.4, 0.2, 0);
+	Eigen::Vector3d upperRightMouthKEsp(0, 0, 0);
+	Object* upperRightMouth = new Sphere(upperRightMouthRadius, upperRightMouthCenter, upperRightMouthKAmbient, upperRightMouthKDif, upperRightMouthKEsp, 0);
+
+	double rightMouthRadius = 10;
+	Eigen::Vector3d rightMouthCenter(-12, 5, -1005);
+	Eigen::Vector3d rightMouthKAmbient(0.4, 0.2, 0);
+	Eigen::Vector3d rightMouthKDif(0.4, 0.2, 0);
+	Eigen::Vector3d rightMouthKEsp(0, 0, 0);
+	Object* rightMouth = new Sphere(rightMouthRadius, rightMouthCenter, rightMouthKAmbient, rightMouthKDif, rightMouthKEsp, 0);
+
+	double leftMouthRadius = 10;
+	Eigen::Vector3d leftMouthCenter(12, 5, -1005);
+	Eigen::Vector3d leftMouthKAmbient(0.4, 0.2, 0);
+	Eigen::Vector3d leftMouthKDif(0.4, 0.2, 0);
+	Eigen::Vector3d leftMouthKEsp(0, 0, 0);
+	Object* leftMouth = new Sphere(leftMouthRadius, leftMouthCenter, leftMouthKAmbient, leftMouthKDif, leftMouthKEsp, 0);
+
+	double upperLeftMouthRadius = 10;
+	Eigen::Vector3d upperLeftMouthCenter(32, 15, -1005);
+	Eigen::Vector3d upperLeftMouthKAmbient(0.4, 0.2, 0);
+	Eigen::Vector3d upperLeftMouthKDif(0.4, 0.2, 0);
+	Eigen::Vector3d upperLeftMouthKEsp(0, 0, 0);
+	Object* upperLeftMouth = new Sphere(upperLeftMouthRadius, upperLeftMouthCenter, upperLeftMouthKAmbient, upperLeftMouthKDif, upperLeftMouthKEsp, 0);
+	
+	scene.addObject(body);
+	scene.addObject(head);
+	scene.addObject(leftEye);
+	scene.addObject(rightEye);
+	scene.addObject(upperRightMouth);
+	scene.addObject(upperLeftMouth);
+	scene.addObject(rightMouth);
+	scene.addObject(leftMouth);
 
 	Eigen::Vector3d floorNormal(0, 1, 0);
-	Eigen::Vector3d floorCenter(0, -90, 0);
+	Eigen::Vector3d floorCenter(0, -200, 0);
 	Eigen::Vector3d floorKAmbient(0.2, 0.7, 0.2);
 	Eigen::Vector3d floorKDif(0.2, 0.7, 0.2);
 	Eigen::Vector3d floorKEsp(0, 0, 0);
@@ -81,22 +138,21 @@ int main(int argc, char* argv[])
 	Object* floor = new Plane(floorNormal, floorCenter, floorKAmbient, floorKDif, floorKEsp, floorSpecularIndex);
 
 	Eigen::Vector3d wallNormal(0, 0, 1);
-	Eigen::Vector3d wallCenter(0, 0, -200);
-	Eigen::Vector3d wallKAmbient(0.3, 0.3, 0.3);
+	Eigen::Vector3d wallCenter(0, 0, -2000);
+	Eigen::Vector3d wallKAmbient(0, 0.5, 0.8);
 	Eigen::Vector3d wallKDif(0.3, 0.3, 0.3);
 	Eigen::Vector3d wallKEsp(0, 0, 0);
 	int wallSpecularIndex = 0;
 	Object* wall = new Plane(wallNormal, wallCenter, wallKAmbient, wallKDif, wallKEsp, wallSpecularIndex);
 
-	scene.addObject(sphere);
 	scene.addObject(floor);
 	scene.addObject(wall);
 
 	Eigen::Vector3d pontualIntensity(0.7, 0.7, 0.7);
-	Eigen::Vector3d pontualOrigin(0, 60, -30);
+	Eigen::Vector3d pontualOrigin(-500, 500, 0);
 	LightSource* pontual = new PontualSource(pontualOrigin, pontualIntensity);
 
-	Eigen::Vector3d ambientIntensity(0.3, 0.3, 0.3);
+	Eigen::Vector3d ambientIntensity(0.55, 0.55, 0.55);
 	LightSource* ambient = new AmbientSource(ambientIntensity);
 
 	scene.addSource(pontual);
